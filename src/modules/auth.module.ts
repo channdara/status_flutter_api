b207b008bd80911e1328cc4bc1f44ca92ccf5c86
channdara <channdara@mango-byte.com>
2020-10-20 11:17:55 +0700

    remove redis

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -4,12 +4,15 @@ import { AuthController } from '../controllers/auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { AuthStrategy } from '../strategies/auth.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { api_secret_key } from '../constants/api.constant';
 
 @Module({
   providers: [AuthService, AuthStrategy],
   controllers: [AuthController],
   imports: [
     TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.register({ secret: api_secret_key }),
   ],
 })
 export class AuthModule {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,24 +4,23 @@ import { Repository } from 'typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { AuthValidation } from '../validations/auth.validation';
 import * as bcrypt from 'bcrypt';
-import { api_secret_key } from '../constants/api.constant';
-import * as redis from 'redis';
-import JWTR from '@chantouchsek/jwt-redis';
 import { MessageConstant } from '../constants/message.constant';
 import { payload } from '../utils/response.util';
-
-const jwtr = new JWTR(redis.createClient());
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(UserEntity) private repo: Repository<UserEntity>) {
+  constructor(
+    @InjectRepository(UserEntity) private repo: Repository<UserEntity>,
+    private jwtService: JwtService,
+  ) {
   }
 
   async login(body: AuthValidation): Promise<any> {
     const user = await this.repo.findOne({ where: { email: body.email } });
     if (user == undefined) throw MessageConstant.invalid_email;
     if (!(await bcrypt.compare(body.password, user.password))) throw MessageConstant.incorrect_password;
-    return { 'access_token': await jwtr.sign(payload(user), api_secret_key), 'user': user };
+    return { 'access_token': await this.jwtService.sign(payload(user)), 'user': user };
   }
 
 }
