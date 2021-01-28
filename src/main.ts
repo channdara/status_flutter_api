2aa7bc51bdf41d2ae7b037b2234fbd3e497596f1
channdara <channdara@mango-byte.com>
2021-01-28 17:53:44 +0700

    remove hostname

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { MainModule } from './main.module';
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap().then(() => Logger.log(`Server is running on ${process.env.API_URL}`, 'Bootstrap'));
